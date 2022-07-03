d8882e1ed37c45ce5c2918c6ec81769cdae6e748
Dhaesha M <dhaesha1@gmail.com>
2022-07-03 17:18:35 -0400

    something must be outdated in my 3d packages bc I had to add a legacy script to my json file in order to run npm start server. little 3d scene is visible so I'm moving on

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import "./index.css";
+import React from "react";
+import { useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function App() {
-  return <div className="App">React App</div>;
+  const ref = useRef();
+  return (
+    <div className="App">
+      <Canvas>
+        <mesh ref={ref}>
+          <boxGeometry attach="geometry" args={[1, 1, 1]} />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
