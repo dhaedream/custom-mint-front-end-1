18805a36496d18e039e195376d99f6e5056cb9b3
Dhaesha M <dhaesha1@gmail.com>
2022-07-03 22:15:23 -0400

    set up components to be imported with the pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./index.css";
 import React from "react";
 import { useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
+import Navbar from "./components/navbar/Navbar.jsx";
 // import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function App() {
   const ref = useRef();
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Navbar />
+      <h1>Mint</h1>
       <Canvas>
         <mesh ref={ref}>
           <boxGeometry attach="geometry" args={[2, 2, 2]} />
         </mesh>
       </Canvas>
-      <h2>1 2 3!</h2>
+      <h1>Customize</h1>
     </div>
   );
 }
